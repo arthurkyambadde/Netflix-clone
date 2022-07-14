d4fb7fdaa82c348ffb59989a32c25e7b9fbd7837
arthurkyambadde <arthurkyambadde9@gmail.com>
2022-07-14 11:03:54 +0300

    added browse menu to Navigation Links wrapper and browse menu drop down

--- netflixapp/src/Components/NavigationLinks/NavigationLinks.js
+++ netflixapp/src/Components/NavigationLinks/NavigationLinks.js
@@ -1,27 +1,36 @@
 import React from "react";
 import classes from "./NavigationLinks.module.css";
+import BrowseDropDown from "../../Icons/BrowseDropDown";
 
 class NavigationLinks extends React.Component {
   render() {
     return (
-      <ul className={classes["navigationLinks_Container"]}>
-        <li className={classes["navigation_Link"]}>
-          {" "}
-          <a href="./home">Home</a>{" "}
-        </li>
-        <li className={classes["navigation_Link"]}>
-          <a href="./series">Serie</a>
-        </li>
-        <li className={classes["navigation_Link"]}>
-          <a href="./films">Films</a>
-        </li>
-        <li className={classes["navigation_Link"]}>
-          <a href="./latest">Latest</a>
-        </li>
-        <li className={classes["navigation_Link"]}>
-          <a href="./myList">My List</a>
-        </li>
-      </ul>
+      <div className={classes["NavigationLinks_Wrapper"]}>
+        <div className={classes["BrowseMenu_Container"]}>
+          <p className={classes["BrowseMenu_para"]}>Browse</p>
+
+          <BrowseDropDown />
+        </div>
+
+        <ul className={classes["navigationLinks_Container"]}>
+          <li className={classes["navigation_Link"]}>
+            {" "}
+            <a href="./home">Home</a>{" "}
+          </li>
+          <li className={classes["navigation_Link"]}>
+            <a href="./series">Serie</a>
+          </li>
+          <li className={classes["navigation_Link"]}>
+            <a href="./films">Films</a>
+          </li>
+          <li className={classes["navigation_Link"]}>
+            <a href="./latest">Latest</a>
+          </li>
+          <li className={classes["navigation_Link"]}>
+            <a href="./myList">My List</a>
+          </li>
+        </ul>
+      </div>
     );
   }
 }
